02d1805b9feea6f8b1e73d4b43b5c64972202e3c
eloydsdlh <e.desande.2021@alumnos.urjc.es>
2025-08-03 11:27:44 +0200

    Configure and seed test data into the database

--- backend/src/main/java/com/skyapartments/backend/dto/ApartmentDTO.java
+++ backend/src/main/java/com/skyapartments/backend/dto/ApartmentDTO.java
@@ -29,4 +29,37 @@ public class ApartmentDTO {
         this.description = apto.getDescription();
         this.address = apto.getAddress();
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
