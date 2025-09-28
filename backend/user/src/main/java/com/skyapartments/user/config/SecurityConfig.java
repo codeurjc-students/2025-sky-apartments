de56a2f753ab5ed28479506bafc9ea8e757be30a
eloydsdlh <e.desande.2021@alumnos.urjc.es>
2025-09-28 11:51:25 +0200

    Implement login endpoints (/api/v1/auth/*)

---
+++ backend/user/src/main/java/com/skyapartments/user/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.skyapartments.user.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.skyapartments.user.security.jwt.JwtRequestFilter;
+import com.skyapartments.user.security.jwt.UnauthorizedHandlerJwt;
+
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private UnauthorizedHandlerJwt unauthorizedHandlerJwt;
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .formLogin(form -> form.disable())
+            .httpBasic(basic -> basic.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .exceptionHandling(ex -> ex.authenticationEntryPoint(unauthorizedHandlerJwt))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/api/users/email").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/users/private/*").permitAll()
+                // PRIVATE ENDPOINTS
+                .requestMatchers(HttpMethod.GET, "/api/users/*").hasAnyRole("USER","ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/users/*").hasAnyRole("USER","ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/users/*").hasAnyRole("USER","ADMIN")
+                // PUBLIC ENDPOINTS
+                
+                .anyRequest().permitAll()
+            );
+
+        // Filter JWT
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
+
